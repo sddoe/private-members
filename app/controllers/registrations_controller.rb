d635ffc62fed2e3e7901088f03b2737b8cde6dc2
Sergio D Perdomo <sergio.931129@gmail.com>
2021-03-15 05:10:41 -0500

    Fix rubocop and linters errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,11 @@
-class RegistrationsController < Devise::RegistrationsController 
+class RegistrationsController < Devise::RegistrationsController
+  private
 
-	private
+  def sign_up_params
+    params.require(:member).permit(:name, :email, :password, :password_confirmation)
+  end
 
-	def sign_up_params 
-		params.require(:member).permit(:name,:email, :password, :password_confirmation)
-	end
-
-	def acount_update_params 
-		params.require(:member).permit(:name,:email, :password, :password_confirmation, :current_password)
-	end
-
+  def acount_update_params
+    params.require(:member).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
 end
