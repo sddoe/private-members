6883ef58de42b1074e0db3d79efa2405587ee82a
Sergio D Perdomo <sergio.931129@gmail.com>
2021-03-15 03:44:36 -0500

    Edit routes and add field to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_082807) do
+ActiveRecord::Schema.define(version: 2021_03_15_084058) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_082807) do
     t.text "post"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "member_id"
   end
 
 end
