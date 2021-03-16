65a00fc0fc2714ced0edc8ea3a26a09a4f9bb516
Sergio D Perdomo <sergio.931129@gmail.com>
2021-03-16 18:44:37 -0500

    Fix rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
-	def show_sign_in_up
-		out = ''
+  def show_sign_in_up
+    out = ''
 
-		if member_signed_in?
-			out << link_to(current_member.name, edit_member_registration_path)
-			out << "</br>"
-			out << link_to('Sign Out', destroy_member_session_path, method: :delete)
-		else
-			out << link_to('Sign In', new_member_session_path)
-			out << "</br>"
-			out << link_to('Sign Up', new_member_registration_path)
-		end
+    if member_signed_in?
+      out << link_to(current_member.name, edit_member_registration_path)
+      out << '</br>'
+      out << link_to('Sign Out', destroy_member_session_path, method: :delete)
+    else
+      out << link_to('Sign In', new_member_session_path)
+      out << '</br>'
+      out << link_to('Sign Up', new_member_registration_path)
+    end
 
-		out.html_safe
-	end
+    out.html_safe
+  end
 end
