9ac8e8d92c5b5106f88408e75247f584a15330d6
Sergio D Perdomo <sergio.931129@gmail.com>
2021-03-16 20:13:26 -0500

    Add create_post helper method and modify the view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,11 @@
 module PostsHelper
+  def create_post
+  	out = ''
+  	if member_signed_in?
+  	  out << link_to('New Post', new_post_path)
+  	end
+  	out.html_safe
+  end
   def show_member_name(post)
     out = ''
     out << "<small><em> #{post.member.name} </em></small>" if member_signed_in?
