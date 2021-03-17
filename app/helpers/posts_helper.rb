0a81d76068c48d6bd14cd317f04842172c231f6c
Sergio D Perdomo <sergio.931129@gmail.com>
2021-03-16 20:16:48 -0500

    Fix rubocop errors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,10 @@
 module PostsHelper
   def create_post
-  	out = ''
-  	if member_signed_in?
-  	  out << link_to('New Post', new_post_path)
-  	end
-  	out.html_safe
+    out = ''
+    out << link_to('New Post', new_post_path) if member_signed_in?
+    out.html_safe
   end
+
   def show_member_name(post)
     out = ''
     out << "<small><em> #{post.member.name} </em></small>" if member_signed_in?
